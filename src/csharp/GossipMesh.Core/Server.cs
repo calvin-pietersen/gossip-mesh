c4502d56437c8f48ffb08225c6d96672e75ce237
Calvin Pietersen <calvinpietersen5@gmail.com>
2018-10-13 16:27:45 +1100

    add csharp

---
+++ src/csharp/GossipMesh.Core/Server.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GossipMesh.Core
+{
+    public class Server
+    {
+        public struct UdpState
+        {
+            public UdpClient u;
+            public IPEndPoint e;
+        }
+
+        private int _listenPort;
+
+        public Server(int listenPort)
+        {
+            _listenPort = listenPort;
+        }
+
+        public async Task StartAsync(IEnumerable<string> seeds)
+        {
+            var listener = new UdpClient(_listenPort);
+            var groupEP = new IPEndPoint(IPAddress.Any, _listenPort);
+
+            var state = new UdpState
+            {
+                u = listener,
+                e = groupEP
+            };
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    var request = await listener.ReceiveAsync().ConfigureAwait(false);
+
+                    Console.WriteLine(System.Text.ASCIIEncoding.ASCII.GetString(request.Buffer));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+    }
+}
---
+++ src/csharp/GossipMesh.Seed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using GossipMesh.Core;
+
+namespace GossipMesh.Seed
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var server = new GossipMesh.Core.Server(11000);
+            server.StartAsync();
+
+            while (true)
+            {
+                Console.WriteLine("seed node doing random things");
+                await Task.Delay(100);
+            }
+        }
+    }
+}
